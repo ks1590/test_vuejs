147b778b2394f8cbc3b28553460ed961fb350dbc
kensuke suzuki <35k.0101@gmail.com>
2020-12-09 22:42:12 +0900

    todoリスト追加のフォームを作成

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="titile" name="title" placeholder="Add Todo..." />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import UUID from "vue-uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: Vue.use(UUID),
+        titile: this.title,
+        completed: false
+      };
+      this.$emit("add-todo", newTodo);
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
